13ed7886cd0aadeb45265c0db4fabab6d86ab838
Andre Oliveira <awloliveira89@gmail.git config --global user.name Andr>
2019-07-20 18:10:35 -0300

    add scriptable scripts for direction and options and also theirs scriptable objects

---
+++ Assets/Scripts/Scriptables/ScriptableDirection.cs
@@ -0,0 +1,16 @@
+﻿#pragma warning disable 649
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "Direction", menuName = "Snake/Direction", order = 0)]
+public class ScriptableDirection : ScriptableObject {
+	[SerializeField] private Vector2Int value;
+	[SerializeField] private WorldDirection side;
+
+	public Vector2Int Value {
+		get { return value; }
+	}
+
+	public WorldDirection Side {
+		get { return side; }
+	}
+}
---
+++ Assets/Scripts/Scriptables/ScriptableOptions.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Options", menuName = "Snake/Options", order = 0)]
+public class ScriptableOptions : ScriptableObject {
+	
+	[Range(1,9)] public int gameLevel = 1;
+	[Range(0f,1f)] public float musicVolume = 1f;
+	[Range(0f,1f)] public float sfxVolume = 1f;
+
+}
