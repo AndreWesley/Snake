7000b905fd378bb31fad3bc801fe943e9cda8caf
Andre Oliveira <awloliveira89@gmail.git config --global user.name Andr>
2019-07-20 18:08:32 -0300

    add pool manager and game grid script

---
+++ Assets/Scripts/GameGrid.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+
+public class GameGrid : MonoBehaviour {
+	[SerializeField] private Transform origin;
+	[SerializeField] private int rows, columns;
+	public HashSet<Vector2Int> grid;
+	public List<BoxCollider2D> boxes;
+
+	void Start () {
+		grid = new HashSet<Vector2Int> ();
+		SetupGrid();
+	}
+
+	private void SetupGrid () {
+		for (int r = 0; r < columns; r++) {
+			for (int c = 0; c < rows; c++) {
+				int x = (int) origin.position.x + c;
+				int y = (int) origin.position.y + r;
+				grid.Add (new Vector2Int (x, y));
+
+				BoxCollider2D box = gameObject.AddComponent<BoxCollider2D> ();
+				box.offset = origin.localPosition + new Vector3(c,r,0f);
+				box.size = Vector2.one * .9f;
+				boxes.Add(box);
+			}
+		}
+	}
+
+	public Vector2Int GetRandomFreePoint () {
+		return grid.ElementAt (Random.Range (0, grid.Count));
+	}
+	
+	private void OnTriggerEnter2D (Collider2D other) {
+//		Vector2 pos = other.transform.position;
+//		grid.Add(new Vector2Int((int) pos.x, (int) pos.y));
+
+		for (int i = 0; i < boxes.Count; i++)
+		{
+ 			Vector2 p = (Vector2) other.transform.position;
+
+			print("boxes: " + boxes[i].offset);
+			print("posic: " + new Vector2Int((int) p.x, (int) p.y));
+			print("---------------------------------------");
+			if (boxes[i].offset == (Vector2) (other.transform.position + origin.localPosition)) {
+				boxes[i].size = Vector2.one * 0.5f;
+			}
+		}
+	}
+	private void OnTriggerExit2D (Collider2D other) {
+//		Vector2 pos = other.transform.position;
+//		grid.Remove(new Vector2Int ((int) pos.x, (int) pos.y));
+
+		for (int i = 0; i < boxes.Count; i++)
+		{
+			if (boxes[i].offset == (Vector2) (other.transform.position - origin.localPosition)) {
+				boxes[i].size = Vector2.one * 0.9f;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PoolManager.cs
@@ -0,0 +1,35 @@
+﻿#pragma warning disable 649
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+
+	[SerializeField] private int amount;
+	[SerializeField] private GameObject prefab;
+	[SerializeField] private List<GameObject> pool;
+	
+
+    void Start()
+    {
+		for (int i = 0; i < amount; i++)
+		{
+			pool.Add(Instantiate(prefab, transform) as GameObject);
+			pool[i].SetActive(false);
+		}
+        
+    }
+
+	public T Spawn<T>(Vector3Int position) where T : Object {
+		for (int i = 0; i < amount; i++)
+		{
+			if (!pool[i].activeInHierarchy) {
+				pool[i].transform.position = position;
+				pool[i].SetActive(true);
+				return pool[i].GetComponent<T>();
+			}
+		}
+		return null;
+	}
+}
