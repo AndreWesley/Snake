addc7ba4ed14a105f76da4fb7d25ad1cbd7443f4
Andre Oliveira <awloliveira89@gmail.git config --global user.name Andr>
2019-07-22 01:52:10 -0300

    add snake game event system

---
+++ Assets/Scripts/Events/GameEventListener.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour {
+	[SerializeField] private ScriptableGameEvent gameEvent = null;
+	[SerializeField] private UnityEvent response = null;
+
+	private void OnEnable () {
+		gameEvent.AddListener (this);
+	}
+
+	private void OnDisable () {
+		gameEvent.RemoveListener (this);
+	}
+
+	public void OnEventCalled () {
+		response.Invoke ();
+	}
+}
---
+++ Assets/Scripts/Events/ScriptableGameEvent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameEvent", menuName = "Snake/GameEvent", order = 3)]
+public class ScriptableGameEvent : ScriptableObject {
+	
+	[SerializeField] private List<GameEventListener> listeners = null;
+	
+	public void Call() {
+		for (int i = 0; i < listeners.Count; i++)
+		{
+			listeners[i].OnEventCalled();
+		}
+	}
+
+	public void AddListener(GameEventListener listener) {
+		listeners.Add(listener);
+	}
+
+	public void RemoveListener(GameEventListener listener) {
+		listeners.Remove(listener);
+	}
+}
