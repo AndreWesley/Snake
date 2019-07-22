21e63e84d9b041685cfa5e9ab7b12d21450c8509
Andre Oliveira <awloliveira89@gmail.git config --global user.name Andr>
2019-07-22 01:57:07 -0300

    update spritesheet config, credits, upadte tail prefab

---
+++ Assets/Credits.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    /*
+	snake arts n sounds = https://opengameart.org/content/snake-sprites-sound
+	 */
+}
