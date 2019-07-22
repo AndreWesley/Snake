a8fbbb91a8a6ccedbb7d56e7ab741218e61a2d30
Andre Oliveira <awloliveira89@gmail.git config --global user.name Andr>
2019-07-22 01:53:39 -0300

    add constants and score script

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+	[SerializeField] private Text scoreText;
+	[SerializeField] private ScriptableScore score;
+	[SerializeField] private ScriptableOptions options;
+
+	public void AddScore() {
+		score.value += options.gameLevel;
+		scoreText.text = score.value.ToString();
+	}
+
+}
