ebe6af67a417c0f7c662aaadebe928729d2c18ba
Andre Oliveira <awloliveira89@gmail.git config --global user.name Andr>
2019-07-22 01:53:04 -0300

    add score and grid script

---
+++ Assets/Scripts/ScriptableGrid.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "ScriptableGrid", menuName = "Snake/ScriptableGrid", order = 0)]
+public class ScriptableGrid : ScriptableObject {
+
+	private HashSet<Vector2Int> avaliableCells = default (HashSet<Vector2Int>);
+
+	public void GenerateGrid (Vector2Int origin, int rows, int columns) {
+		avaliableCells = new HashSet<Vector2Int> ();
+		for (int r = 0; r < rows; r++) {
+			for (int c = 0; c < columns; c++) {
+				int x = origin.x + c;
+				int y = origin.y + r;
+				avaliableCells.Add (new Vector2Int (x, y));
+			}
+		}
+	}
+
+	public Vector2Int GetRandomFreePoint () {
+		return avaliableCells.ElementAt (Random.Range (0, avaliableCells.Count));
+	}
+
+	public void Remove (Vector2Int cell) {
+		avaliableCells.Remove (cell);
+	}
+
+	public void Remove (Vector3Int cell) {
+		avaliableCells.Remove (new Vector2Int (cell.x, cell.y));
+	}
+
+	public void Add (Vector2Int cell) {
+		avaliableCells.Add (cell);
+	}
+
+	public void DrawGizmos () {
+		Gizmos.color = new Color (1f, 0f, 0f, .25f);
+		if (avaliableCells != default (HashSet<Vector2Int>)) {
+			foreach (var item in avaliableCells) {
+				Vector3 v = new Vector3 (item.x, item.y, 1f);
+				Gizmos.DrawCube (v, Vector3.one * 0.9f);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/ScriptableScore.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptableScore", menuName = "Snake/ScriptableScore", order = 5)]
+public class ScriptableScore : ScriptableObject, ISerializationCallbackReceiver {
+	public int value;
+	public int highScore;
+
+	public void OnAfterDeserialize() {}
+
+	public void OnBeforeSerialize() {
+		value = 0;
+	}
+
+	public bool CheckNewRecord() {
+		if (value > highScore) {
+			highScore = value;
+			return true;
+		}
+		return false;
+	}
+}
